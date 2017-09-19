9d179833950777d5493145295347d30439d8a6cb
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-19 11:45:34 -0400

    Projet est maintenant Spring.

---
+++ src/main/java/com/example/demo/ProjetMultitiersApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMultitiersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMultitiersApplication.class, args);
+	}
+}
