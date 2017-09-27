868057717d01f566d94c13728b98231a19f7f944
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-27 19:47:23 -0400

    EQ1-53: Node modules installes et fonctionnels. Node app de base.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+import './App.css';
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+				
+		}
+	}
+	render(){
+		return(
+				<div>
+				Test
+				</div>
+				);
+	}
+}
+
+
+
+export default App;
