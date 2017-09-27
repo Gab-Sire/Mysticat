72abe7c4e148279d1900c777d27683a492e51b82
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-26 20:18:57 -0400

    EQ1-49: Fonctions objets metiers.

--- src/main/java/com/multitiers/service/Player.java
+++ src/main/java/com/multitiers/service/Player.java
@@ -3,6 +3,7 @@ package com.multitiers.service;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.multitiers.util.Constantes;
@@ -20,9 +21,14 @@ public class Player {
 		this.graveyard = new ArrayList<PlayableCard>();
 		this.field = new Minion[Constantes.MAX_FIELD_SIZE];
 		this.hand = new ArrayList<PlayableCard>();
-		this.deck = new ArrayList<PlayableCard>(); //Doit etre remplit
-		this.remainingMana = Constantes.STARTING_MANA; //Probablement assigned par le jeu
+		this.deck = new ArrayList<PlayableCard>(); 
 		this.fatigueDamage = Constantes.STARTING_FATIGUE_DAMAGE;
+		this.remainingMana = Constantes.STARTING_MANA; //Probablement assigned par le jeu
+		
+		Collections.shuffle(deck);
+		for(int i=0; i<Constantes.STARTING_HAND_SIZE; i++) {
+			drawCard();
+		}
 	}
 	
 	public void drawCard() {
@@ -30,7 +36,6 @@ public class Player {
 			takeFatigueDamage();
 			return;
 		}
-		
 		PlayableCard cardDrawn = deck.get(0);
 		deck.remove(0);
 		if(hand.size()<Constantes.MAX_HAND_SIZE) {
@@ -42,4 +47,63 @@ public class Player {
 		this.hero.health-= fatigueDamage;
 		fatigueDamage++;
 	}
+
+	public Hero getHero() {
+		return hero;
+	}
+
+	public void setHero(Hero hero) {
+		this.hero = hero;
+	}
+
+	public List<PlayableCard> getGraveyard() {
+		return graveyard;
+	}
+
+	public void setGraveyard(List<PlayableCard> graveyard) {
+		this.graveyard = graveyard;
+	}
+
+	public Minion[] getField() {
+		return field;
+	}
+
+	public void setField(Minion[] field) {
+		this.field = field;
+	}
+
+	public List<PlayableCard> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(List<PlayableCard> deck) {
+		this.deck = deck;
+	}
+
+	public List<PlayableCard> getHand() {
+		return hand;
+	}
+
+	public void setHand(List<PlayableCard> hand) {
+		this.hand = hand;
+	}
+
+	public Integer getRemainingMana() {
+		return remainingMana;
+	}
+
+	public void setRemainingMana(Integer remainingMana) {
+		this.remainingMana = remainingMana;
+	}
+
+	public Integer getFatigueDamage() {
+		return fatigueDamage;
+	}
+
+	public void setFatigueDamage(Integer fatigueDamage) {
+		this.fatigueDamage = fatigueDamage;
+	}
+	
+	
+	
 }
