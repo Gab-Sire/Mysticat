ceddb483fa43c7189eb2400b9040b23c910e0a99
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-28 11:49:34 -0400

    EQ1-48: Faire un rectangle en react.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React, {Component} from 'react';
 import _ from 'lodash';
 import './App.css';
 
+const player = {
+		id: 1,
+		name: "ChatOne",
+		hand:[
+			{
+				manaCost : 1,
+				power: 1,
+				health: 1,
+				speed : 1
+			},
+			{
+				manaCost : 2,
+				power: 2,
+				health: 2,
+				speed : 2
+			},
+			{
+				manaCost : 3,
+				power: 3,
+				health: 3,
+				speed : 3
+			}
+		]
+}
+
+
 class App extends Component{
 	constructor(props){
 		super(props);
@@ -11,8 +37,8 @@ class App extends Component{
 	}
 	render(){
 		return(
-				<div>
-				Test
+				<div id="board">
+				<CreateHand hand={this.state.hand} createCard={this.createTask.bind(this)}/>
 				</div>
 				);
 	}
