275b3dfdc83b785a2ab5d5ace49e4398d311291f
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-29 18:51:06 -0400

    Exemple d'affichage avec React.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import React, {Component} from 'react';
 import _ from 'lodash';
 import './App.css';
 
-const player = {
+const self = {
 		id: 1,
 		name: "ChatOne",
-		hand:[
+		remainingHealth : 30,
+		remainingMana : 1,
+		selfHand:[
 			{
+				name: "Chat One",
 				manaCost : 1,
 				power: 1,
 				health: 1,
 				speed : 1
 			},
 			{
+				name: "Chat Two",
 				manaCost : 2,
 				power: 2,
 				health: 2,
 				speed : 2
 			},
 			{
+				name: "Chat Three",
 				manaCost : 3,
 				power: 3,
 				health: 3,
 				speed : 3
 			}
-		]
+		],
+		selfDeck:[],
+		selfGraveyard:[],
+		
 }
 
 
@@ -38,7 +46,22 @@ class App extends Component{
 	render(){
 		return(
 				<div id="board">
-					Wow
+					<div id="opponentHand">
+						<div className="card">
+							Card back
+						</div>
+					</div>
+					<div id="selfHand">
+						<div className="card">
+							<div className="cardSpeed">
+							Chat One
+							Cost: 1
+							Power: 10
+							Health: 8
+							Speed: 10
+							</div>
+						</div>
+					</div>
 				</div>
 				);
 	}
