fd7663da7d4215fe54bb92c0f457ae2de13e5358
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-09-30 20:06:55 -0400

    EQ1-51: Affichage des points de vie des 2 joueurs et affichage de la mana globale (par tour).

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import _ from 'lodash';
 import './App.css';
 
 
@@ -27,6 +26,22 @@ class App extends Component{
 
 							}
 						]
+						},
+						{
+							hero:{
+								health:30
+							},
+							hand : [
+								{
+									name: "test",
+									initialPower: "test",
+									manaCost: "test",
+									initialHealth: "test",
+									initialSpeed: "test",
+									description: "test"
+
+								}
+							]
 						}
 				]
 			}
@@ -35,6 +50,8 @@ class App extends Component{
 	render(){
 		let selfHand = this.state.gameState.players[0].hand;
 		let selfHealth = this.state.gameState.players[0].hero.health;
+		let opponentHealth = this.state.gameState.players[1].hero.health;
+		let globalMana = this.state.gameState.currentMana;
 			return(
 				<div>
 					<div id="board">
@@ -43,17 +60,25 @@ class App extends Component{
 								Card back
 							</div>
 						</div>
+						<div id="opponentHealth" className="heroHealth">
+						{opponentHealth}
+						</div>
+						<div id="globalMana" title="The amount of mana that you get per turn">
+							<div>
+								{globalMana}
+							</div>
+						</div>
 							<div id="selfHealth" className="heroHealth">
-							{selfHealth}
+								{selfHealth}
 							</div>
 							<div id="selfHand" className="hand">
 							{selfHand.map((card)=>{
 								return <div className="card" title={card.description}>
 								<div className="cardName">{card.name}</div>
-								<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {card.manaCost}</div>
-								<div title="The amount of damage this minion deals" className="cardPower">Power: {card.initialPower}</div>
-								<div title="The amount of damage this minion can take" className="cardHealth">Health: {card.initialHealth}</div>
-								<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {card.initialSpeed}</div>
+									<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {card.manaCost}</div>
+									<div title="The amount of damage this minion deals" className="cardPower">Power: {card.initialPower}</div>
+									<div title="The amount of damage this minion can take" className="cardHealth">Health: {card.initialHealth}</div>
+									<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {card.initialSpeed}</div>
 								</div>
 							})}
 						</div>
@@ -67,7 +92,7 @@ class App extends Component{
 		axios({
 			  method:'get',
 			  url:'http://localhost:8089/getHardCodedGame',
-			  responseType:'stream',
+			  responseType:'json',
 			  headers: {'Access-Control-Allow-Origin': "true"}
 			})
 			  .then((response)=>{
