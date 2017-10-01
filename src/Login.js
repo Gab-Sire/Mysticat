a47199bfc95eed360d53b7a5f959d54fc981f10b
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-10-01 14:40:01 -0400

    EQ1-56: Creation du fichier Login.js

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './App.css';
+
+export default class Login extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+				
+		}
+	}
+}
