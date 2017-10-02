a042f1d970379c231ca58efb69694cab39ec12d6
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-10-02 10:38:46 -0400

    Fixed h2 console not showing.

--- src/main/java/com/multitiers/config/RestSecurityConfig.java
+++ src/main/java/com/multitiers/config/RestSecurityConfig.java
@@ -7,6 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+    http.csrf().disable().headers().frameOptions().disable();
   }
 }
