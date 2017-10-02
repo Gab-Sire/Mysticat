f87722797b5f5544d04bb99ecf1e9f9f9b695ad3
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-10-02 14:18:30 -0400

    Supprimer des imports non utilises.

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,6 @@ export default class Card extends Component{
 	constructor(props){
 		super(props);
 		this.state={
-		
 		}
 	}
 	
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import './app.css';
 
 export default class Login extends Component{
