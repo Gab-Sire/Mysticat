9b2782f61288e1872c52a2480b20b6763423bd05
cyr_gabriel <cyr.gabriel@hotmail.com>
2017-10-05 18:38:41 -0400

    EQ1-55 rendement conditionnel de la carte terminee

--- src/Card.js
+++ src/Card.js
@@ -3,17 +3,15 @@ import React, {Component} from 'react';
 export default class Card extends Component{
 	constructor(props){
 		super(props);
-		this.state={
-			faceUp : true
-		}
+		this.state={faceUp: true};
 	}
 	
 	render(){
 		let isFaceUp = this.state.faceUp;
 		
 		if(isFaceUp){
-			return (<div className="card" title={this.state.description}>
-			<div className="cardName">{this.props.name}</div>
+			return (<div className="card" title={this.state.description}}>
+			<div className="cardName">{this.props.name}op</div>
 				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
 				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
 				<div title="The amount of damage this minion can take" className="cardHealth">Health: {this.props.initialHealth}</div>
@@ -21,16 +19,7 @@ export default class Card extends Component{
 			</div>);
 		}
 		else{
-			return (<div className="card" title={this.state.description}>
-			<div className="cardName">{this.props.name}</div>
-				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
-				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
-				<div title="The amount of damage this minion can take" className="cardHealth">Health: {this.props.initialHealth}</div>
-				<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {this.props.initialSpeed}</div>
-			</div>);
-		}
-		
+			return (<div className="cardFacedDown"></div>);
+		}	
 	}
-	
-	
 }
