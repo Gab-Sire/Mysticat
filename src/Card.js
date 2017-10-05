016f94e5889a9e3effe0b1b337f0a44c2e965ff5
cyr_gabriel <cyr.gabriel@hotmail.com>
2017-10-05 18:07:01 -0400

    EQ1-55 introduction de la classe css cardFcedDown pour afficher une carte de dos, insertion d'une variable de state pour faire un rendement conditionnel

--- src/Card.js
+++ src/Card.js
@@ -4,16 +4,33 @@ export default class Card extends Component{
 	constructor(props){
 		super(props);
 		this.state={
+			faceUp : true
 		}
 	}
 	
 	render(){
-		return (<div className="card" title={this.state.description}>
+		let isFaceUp = this.state.faceUp;
+		
+		if(isFaceUp){
+			return (<div className="card" title={this.state.description}>
 			<div className="cardName">{this.props.name}</div>
 				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
 				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
 				<div title="The amount of damage this minion can take" className="cardHealth">Health: {this.props.initialHealth}</div>
 				<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {this.props.initialSpeed}</div>
 			</div>);
+		}
+		else{
+			return (<div className="card" title={this.state.description}>
+			<div className="cardName">{this.props.name}</div>
+				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
+				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
+				<div title="The amount of damage this minion can take" className="cardHealth">Health: {this.props.initialHealth}</div>
+				<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {this.props.initialSpeed}</div>
+			</div>);
+		}
+		
 	}
+	
+	
 }
