ee16e9ec24b296d4dc719740238aabb7c7409706
cyr_gabriel <cyr.gabriel@hotmail.com>
2017-10-07 17:00:40 -0400

    ajout d'un min-width pour l'app et ajout d'une fonctionnalité pour sélectionner une carte dans Card.js

--- src/cardComponents/Card.js
+++ src/cardComponents/Card.js
@@ -3,16 +3,24 @@ import React, {Component} from 'react';
 export default class Card extends Component{
 	constructor(props){
 		super(props);
-		this.state={faceUp: true};
+		this.state={faceUp: true, selected:false};
 	}
 	
+	handleClick = () => {
+		this.setState(prevState =>({ selected: !this.state.selected }));
+	};
+	
 	render(){
+		let isSelected = "";
 		if(this.props.faceUp != null && this.props.faceUp != undefined){
 			this.state.faceUp = this.props.faceUp;
 		}
+		if(true == this.state.selected){
+			isSelected = "selected";
+		}
 
 		if(true == this.state.faceUp){
-			return (<div className="card" title={this.state.description}>
+			return (<div className={"card " + isSelected} title={this.state.description} onClick={this.handleClick}>
 				<div className="cardName">{this.props.name}</div>
 				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
 				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
