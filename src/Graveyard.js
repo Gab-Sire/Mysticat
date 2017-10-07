de412538a4eb4bb35e024e4332ec2a8c1f4547e4
cyr_gabriel <cyr.gabriel@hotmail.com>
2017-10-07 14:25:20 -0400

    EQ1-62 rendering conditionnel

--- src/Graveyard.js
+++ src/Graveyard.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import CardTile from './CardTile.js';
+import Card from './Card.js';
 
 export default class Graveyard extends Component{
 	constructor(props){
 		super(props);
+		this.state={ isEmpty:true};
 	}
 	
 	render(){
-		return <div id={this.props.id} className="graveyard">
-			   		<CardTile />
-			   	</div>	
+		if(this.props.isEmpty != null && this.props.isEmpty != undefined){
+			this.state.isEmpty = this.props.isEmpty;
+		}
+		
+		if(true == this.state.isEmpty){
+			return (<div id={this.props.id} className="graveyard">
+	   			<CardTile />
+	   		</div>);
+		}
+		else{
+			return (<div id={this.props.id} className="graveyard">
+	   			<Card faceUp="false" />
+	   		</div>);
+		}
 	}
 }
