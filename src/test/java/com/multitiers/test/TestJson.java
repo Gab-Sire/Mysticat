ac175854fa33060ae7ce9e1a101b59e124044789
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-10-16 11:22:16 -0400

    Test unitaire pour serialisation et deserialisation des UserCredentials.

--- src/test/java/com/multitiers/test/TestJson.java
+++ src/test/java/com/multitiers/test/TestJson.java
@@ -6,43 +6,49 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.multitiers.domaine.entity.UserCredentials;
 import com.multitiers.util.JsonUtils;
 
 public class TestJson {
 
-
 	String invalidJson;
 	String userCredentialsJson;
 	UserCredentials userCredentials;
-	
+
 	@Before
-	public void setUp() throws Exception{
-		userCredentialsJson= "{username: 'allo', password:'myboy'}";
+	public void setUp() throws Exception {
+		userCredentialsJson = "{\"username\":\"allo\",\"password\":\"myboy\"}";
 		invalidJson = "uiouiasiolasd//";
 		userCredentials = new UserCredentials();
 		userCredentials.setUsername("allo");
 		userCredentials.setPassword("myboy");
 	}
-	
+
 	@After
-	public void tearDown() throws Exception{
+	public void tearDown() throws Exception {
 		userCredentialsJson = null;
 	}
-	
-	
-	
+
 	@Test
 	public void deserializingValidUserCredentialsJsonReturnsAValidUserCredentialsObject() {
 		UserCredentials userCredentialsFromJson = JsonUtils.deserializeUserCredentialsFromJson(userCredentialsJson);
-		assertTrue(userCredentials.getUsername().equals(userCredentialsFromJson.getUsername()) 
+		assertTrue(userCredentials.getUsername().equals(userCredentialsFromJson.getUsername())
 				&& userCredentials.getPassword().equals(userCredentialsFromJson.getPassword()));
 	}
-	
-	@Test(expected=JsonSyntaxException.class)
+
+	@Test(expected = JsonSyntaxException.class)
 	public void deserializingInvalidUserCredentialsThrowsJsonException() {
 		UserCredentials userCredentialsFromJson = JsonUtils.deserializeUserCredentialsFromJson(invalidJson);
+	}
+	
+	@Test
+	public void serializingUserCredentialsAndDeserializingItBackEqualsTheSameObject() {
+		String userCredentialsConvertedToJson = new Gson().toJson(userCredentials);
+		UserCredentials backFromJson = JsonUtils.deserializeUserCredentialsFromJson(userCredentialsConvertedToJson);
+		assertTrue(userCredentials.getUsername().equals(backFromJson.getUsername())
+				&& userCredentials.getPassword().equals(backFromJson.getPassword()));
 	}
 
 }
