bd7b1929ea1a80e9fc045ca103e190d88d1fac6a
Jimmy <jimmyetlim@gmail.com>
2017-10-26 19:49:37 -0400

    Minion Mana Cristal Mod

--- src/cardComponents/Card.js
+++ src/cardComponents/Card.js
@@ -14,8 +14,8 @@ export default class Card extends Component{
 
 		if(true === this.props.faceUp){
 			return (<div className={"card " + isSelected} title={this.props.description} onClick={this.props.onClick}>
+				<div className='cardManaContainer' title="The amount of mana crystals consumed when summoning this minion"><div className="cardManaCost">{this.props.manaCost}</div></div>
 				<div className="cardName">{this.props.name}</div>
-				<div title="The amount of mana crystals consumed when summoning this minion" className="cardManaCost">Cost: {this.props.manaCost}</div>
 				<div title="The amount of damage this minion deals" className="cardPower">Power: {this.props.initialPower}</div>
 				<div title="The amount of damage this minion can take" className="cardHealth">Health: {this.props.initialHealth}</div>
 				<div title="Speed dictates the order in which attacks resolve" className="cardSpeed">Speed: {this.props.initialSpeed}</div>
