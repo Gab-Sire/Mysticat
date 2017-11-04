7ae7e0e1b7653c253b835eb1935874e074b43104
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-11-04 13:39:02 -0400

    Tests individuels pour le tri de cartes par nom et mana cost.

--- src/test/java/com/multitiers/test/DomainTest.java
+++ src/test/java/com/multitiers/test/DomainTest.java
@@ -54,7 +54,86 @@ public class DomainTest {
 		Collections.sort(cardListTest);
 		
 		assertThat(cardListTest).isEqualTo(expectedCardList);
+	}
+	
+	@Test
+	public void testSortingCardsByName() {
+		List<Card> cardListTest = new ArrayList<>();
+		List<Card> expectedCardList = new ArrayList<>();
 		
+		Card card01 = new MinionCard();
+		Card card02 = new MinionCard();
+		Card card03 = new MinionCard();
+		Card card04 = new MinionCard();
+		Card card05 = new MinionCard();
+		
+		card01.setManaCost(0);
+		card02.setManaCost(0);
+		card03.setManaCost(0);
+		card04.setManaCost(0);
+		card05.setManaCost(0);
+		
+		card01.setCardName("a");
+		card02.setCardName("A");
+		card03.setCardName("Aa");
+		card04.setCardName("aaA");
+		card05.setCardName("aa");
+		
+		cardListTest.add(card01);
+		cardListTest.add(card02);
+		cardListTest.add(card03);
+		cardListTest.add(card04);
+		cardListTest.add(card05);
+		
+		expectedCardList.add(card02);
+		expectedCardList.add(card01);
+		expectedCardList.add(card03);
+		expectedCardList.add(card05);
+		expectedCardList.add(card04);
+		
+		Collections.sort(cardListTest);
+		
+		assertThat(cardListTest).isEqualTo(expectedCardList);
+	}
+	
+	@Test
+	public void testSortingCardsByManaCost() {
+		List<Card> cardListTest = new ArrayList<>();
+		List<Card> expectedCardList = new ArrayList<>();
+		
+		Card card01 = new MinionCard();
+		Card card02 = new MinionCard();
+		Card card03 = new MinionCard();
+		Card card04 = new MinionCard();
+		Card card05 = new MinionCard();
+		
+		card01.setManaCost(10);
+		card02.setManaCost(0);
+		card03.setManaCost(1);
+		card04.setManaCost(3);
+		card05.setManaCost(2);
+		
+		card01.setCardName("a");
+		card02.setCardName("a");
+		card03.setCardName("a");
+		card04.setCardName("a");
+		card05.setCardName("a");
+		
+		cardListTest.add(card01);
+		cardListTest.add(card02);
+		cardListTest.add(card03);
+		cardListTest.add(card04);
+		cardListTest.add(card05);
+		
+		expectedCardList.add(card02);
+		expectedCardList.add(card03);
+		expectedCardList.add(card05);
+		expectedCardList.add(card04);
+		expectedCardList.add(card01);
+		
+		Collections.sort(cardListTest);
+		
+		assertThat(cardListTest).isEqualTo(expectedCardList);
 	}
 
 }
