f2851f94cedb6b483c2fe0b47c28b8003767d985
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-11-04 13:01:30 -0400

    Image dans affichage de deck est maintenant centre.

--- src/cardComponents/Card.js
+++ src/cardComponents/Card.js
@@ -25,7 +25,7 @@ export default class Card extends Component{
 			return (<div className={"card"+listedInstance +" "+ isSelected} title={this.props.description} onClick={this.props.onClick}>
 				<div className={'cardDetailContainer'+listedInstance+' cardManaCost'} title="The amount of mana crystals consumed when summoning this minion"><div className=" balancingDetail" >{this.props.manaCost}</div></div>
 				<img src= {'/' + this.props.imagePath}  />
-				<div className="cardName">{cardName}</div>
+				<div className={"cardName"+listedInstance}>{cardName}</div>
 				<div title="The amount of damage this minion deals" className={'cardPower'+listedInstance+" "+bigStat}><div className='placingAttribut'>{this.props.initialPower}</div></div>
 				<div title="The amount of damage this minion can take" className={'cardHealth'+listedInstance+" "+bigStat}><div className="placingAttribut">{this.props.initialHealth}</div></div>
 				<div title="Speed dictates the order in which attacks resolve" className={'cardSpeed'+listedInstance+" "+bigStat}><div className="placingAttribut">{this.props.initialSpeed}</div></div>
