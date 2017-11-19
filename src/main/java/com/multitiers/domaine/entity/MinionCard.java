f44efa0cd6604d8d0bd958c738ed40d2157ec9f9
marcantoinebechard <fatbirdanonymous@gmail.com>
2017-11-19 17:12:21 -0500

    Ajout noms de colonnes pour les MinionCards.

--- src/main/java/com/multitiers/domaine/entity/MinionCard.java
+++ src/main/java/com/multitiers/domaine/entity/MinionCard.java
@@ -1,13 +1,17 @@
 package com.multitiers.domaine.entity;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue(value = "minion")
 public class MinionCard extends Card{
+	@Column(name="car_power")
 	private Integer initialPower;
+	@Column(name="car_health")
 	private Integer initialHealth;
+	@Column(name="car_speed")
 	private Integer initialSpeed;
 	
 	public Integer getInitialPower() {
